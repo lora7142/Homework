343a8d2a1e8c179307f42d7fe1e43dfbb39dc4f8
Lora Paro <loraparo@gmail.com>
2025-05-24 19:19:03 -0500

    Week 4 Homework

---
+++ Week4/index.js
@@ -0,0 +1,3 @@
+document.title = 'Lora on the Loose!';
+
+document.getElementsByTagName("h3")[0].innerHTML = "Here's what the riding lawn mower looked like a few weeks back. Boy, was it a mess to fix!";
